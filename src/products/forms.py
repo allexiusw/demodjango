cdbb7dc4dd8485e03b71ca21f1c123be9a4aaa85
allexiusw <william.al1379@gmail.com>
2020-12-21 18:43:05 -0600

    Set initial user in form when editing

--- src/products/forms.py
+++ src/products/forms.py
@@ -1,4 +1,5 @@
-from django import forms 
+from django import forms
+from django.forms import widgets 
 
 from .models import Product 
 
@@ -6,3 +7,9 @@ class ProductForm(forms.ModelForm):
     class Meta: 
         model = Product 
         fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super(ProductForm, self).__init__(*args, **kwargs)
+        if 'initial' in kwargs:
+            if 'user_id' in kwargs['initial']:
+                self.fields['user'].initial = kwargs['initila']['user_id']
