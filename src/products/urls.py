dac72042de58e9deb1c631f09becfc6c8d1079e4
allexiusw <william.al1379@gmail.com>
2020-12-21 18:09:04 -0600

    implement edit

--- src/products/urls.py
+++ src/products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import  list_products
+from .views import  list_products, delete_product, edit_product
 
 urlpatterns = [
     path('', list_products, name="list_products"),
+    path('delete/<int:pk>', delete_product, name="delete_product"),
+    path('edit/<int:pk>', edit_product, name="edit_product"),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -1,9 +1,11 @@
+from django.core.checks import messages
 from django.shortcuts import redirect, render
-from django.http import HttpResponse, request
+from django.contrib import messages
 from .models import Product
 from .forms import ProductForm
 
 def list_products(request):
+    action = "new"
     if request.method == "GET":
         form = ProductForm()
     else:
@@ -12,4 +14,22 @@ def list_products(request):
             form.save()
             form = ProductForm()
     rows = Product.objects.all()
-    return render(request, "list_products.html", {"rows": rows, 'form': form})
+    return render(request, "list_products.html", {"rows": rows, 'form': form, "action": action})
+
+def delete_product(request, pk):
+    product = Product.objects.get(pk=pk).delete()
+    messages.success(request, "Producto eliminado correctamente")
+    return redirect("list_products")
+
+def edit_product(request, pk):
+    action = "update"
+    if request.method == "GET":
+        product = Product.objects.get(pk=pk)
+        form = ProductForm(instance=product)
+    else:
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('list_products')
+    rows = Product.objects.all()
+    return render(request, "edit_product.html", {"rows": rows, 'form': form, "action": action})
